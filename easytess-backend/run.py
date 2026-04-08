f599f2134e7f125dd89593ecf95d120add0656de
Muhend <hamohand@gmail.com>
2026-04-08 11:45:55 +0200

    Suppression easytess-backend et easytess-frontend

--- easytess-backend/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8082))
-    app.run(debug=True, host='0.0.0.0', port=port)
--- easytess-frontend/src/app/services/index.ts
+++
@@ -1,5 +0,0 @@
-// index.ts - Barrel export pour faciliter les imports
-export * from './models';
-export * from './ocr.service';
-export * from './entity.service';
-export * from './file.service';
